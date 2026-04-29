aa43524fb3d6c0f9d67f91cb689ba88e1d639fb8
Kauan <pireskauan80@gmail.com>
2026-04-29 16:14:41 -0300

    links

---
+++ src/data/Projetos.jsx
@@ -0,0 +1,61 @@
+import proj1 from "../assets/proj1.png";
+import proj2 from "../assets/proj1.png";
+import proj3 from "../assets/proj1.png";
+import proj4 from "../assets/proj1.png";
+import proj5 from "../assets/proj1.png";
+
+const projetos = [
+  {
+    id: 1,
+    nome: "Facilita Pet",
+    descricao: "Site para um petshop com apresentação de serviços.",
+    tecnologias: ["HTML", "CSS"],
+    github: "https://github.com/Humopz/pet-shop",
+    deploy: "https://pet-shop-liard.vercel.app/",
+    imagem: proj1,
+    participacao: "Desenvolvimento do front-end e layout do site."
+  },
+  {
+    id: 2,
+    nome: "TeraLink",
+    descricao: "Sistema com autenticação de usuários.",
+    tecnologias: ["React", "Firebase"],
+    github: "https://github.com/Humopz/TeraLink-Front",
+    deploy: "https://tera-link-front.vercel.app/",
+    imagem: proj2,
+    participacao: "Criação da interface e integração com autenticação."
+  },
+  {
+    id: 3,
+    nome: "PataClinic",
+    descricao: "Aplicação de cálculos básicos.",
+    tecnologias: ["JavaScript", "HTML", "CSS"],
+    github: "https://github.com/Humopz/PI-PataClinic_3DSM",
+    deploy: "https://pet-clinic-chi.vercel.app/",
+    imagem: proj3,
+    participacao: "Lógica completa e interface."
+  },
+  {
+    id: 4,
+    nome: "ECOmida",
+    descricao: "Lista de tarefas com armazenamento.",
+    tecnologias: ["Node.js, JavaScript, css, Git"],
+    github: "https://github.com/Humopz/projeto-integrador-4-semestre",
+    deploy: "https://pi-ecomida4dsm.vercel.app/",
+    imagem: proj4,
+    participacao: "Gerenciamento de estado."
+  },
+  {
+    id: 5,
+    nome: "GeFi",
+    descricao: "Página institucional responsiva.",
+    tecnologias: ["HTML", "CSS"],
+    github: "https://github.com/Humopz/PI-Gefi-5DSM",
+    deploy: "https://frontend-gefi-5-dsm.vercel.app/",
+    imagem: proj5,
+    participacao: "Design e layout."
+  },
+
+];
+
+export default projetos;
